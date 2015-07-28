79bbbfd402ae9425a807f1f8bccd49a130f53137
Matthew Mayer <matthewkmayer@gmail.com>
2015-07-28 16:16:18 -0700

    Fleshes out basic structure more, ready for some implementation.

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 )
 
@@ -20,20 +21,56 @@ func main() {
 
 	setup()
 
-	readFromJSON()
+	// TODO: take in a flag with a file to post.
+	eventJSON, _ := readFileContents("sample-event.json")
 
-	sendToCalagator()
+	eventEntry := readFromJSON(eventJSON)
+
+	// These can be refactored into a single function that calls them all
+	sendToCalagator(eventEntry)
+	sendToMeetup(eventEntry)
+	sendToAgilePDXWebsite(eventEntry)
+	sendToTwitter(eventEntry)
 }
 
 func setup() {
 	// envconfig bits for various integration, such as Meetups API
 }
 
-func readFromJSON() {
-	// read from JSON input file
+func readFileContents(file string) ([]byte, error) {
+	fileAsBytes, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		log.Fatalln("Couldn't open ", file)
+	}
+
+	return fileAsBytes, nil
 }
 
-func sendToCalagator() {
-	log.Println("Sending to calagator...")
-	// Totally sends to calagator
+func readFromJSON(eventJSON []byte) event {
+	var eventEntry event
+	err := json.Unmarshal([]byte(eventJSON), &eventEntry)
+
+	if err != nil {
+		log.Fatal("Couldn't parse JSON file.")
+	}
+	return eventEntry
+}
+
+func sendToCalagator(eventEntry event) {
+	log.Println("Totally sending to calagator...")
+	bleh, _ := json.Marshal(eventEntry)
+	log.Println(string(bleh))
+}
+
+func sendToMeetup(eventEntry event) {
+	log.Println("Totally sending to meetup...")
+}
+
+func sendToAgilePDXWebsite(eventEntry event) {
+	log.Println("Totally sending to agilepdx website...")
+}
+
+func sendToTwitter(eventEntry event) {
+	log.Println("Totally sending to twitter...")
 }
